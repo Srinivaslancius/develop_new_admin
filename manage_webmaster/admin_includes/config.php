53e732a4c7fd4aca73ce0d55f5079bc113a37bbc
Srinivaslancius <srinivas@lanciussolutions.com>
2017-08-29 15:35:20 +0530

    new chnaged updated

---
+++ manage_webmaster/admin_includes/config.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Kolkata");
+
+$setcon = 2;
+if($setcon == 1) {
+	$servername = "localhost";
+	$username = "";
+	$password = "capital@123";
+	$dbname = "capitalsecure_latest";
+} else {
+	$servername = "192.168.0.101";	
+	$username = "root";
+	$password = "root";
+	$dbname = "develop_new_admin";
+}  
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$base_url = "http://localhost/develop_new_admin/";
+
+?>
---
+++ manage_webmaster/admin_includes/footer.php
@@ -0,0 +1,10 @@
+      <div class="site-footer"> Design & Developed By Lancius IT Solutions 
+      </div>
+    </div>
+    <script src="js/vendor.min.js"></script>
+    <script src="js/cosmos.min.js"></script>
+    <script src="js/application.min.js"></script>
+    <script src="js/index.min.js"></script>
+  </body>
+
+</html>
