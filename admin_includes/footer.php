d4c08d6b57dafe75f59c69353b5691989bd2dd47
srinivaslancius <srinivas@lanciussolutions.com>
2017-08-29 15:35:43 +0530

    Changed site settings page and added  footer

--- admin_includes/footer.php
+++ admin_includes/footer.php
@@ -5,6 +5,14 @@
     <script src="js/cosmos.min.js"></script>
     <script src="js/application.min.js"></script>
     <script src="js/index.min.js"></script>
+    <script>
+	function isNumberKey(evt){
+	    var charCode = (evt.which) ? evt.which : event.keyCode
+	    if (charCode > 31 && (charCode < 48 || charCode > 57))
+	        return false;
+	    return true;
+	}
+	</script>
   </body>
 
 </html>
