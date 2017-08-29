48d16e52f235aeb50d9935ce32b5085bd6cb06df
Gunavardhan <vardhan.gujjula@gmail.com>
2017-08-29 13:16:46 +0530

    users changes committed

--- add_admin_users.php
+++ add_admin_users.php
@@ -45,14 +45,15 @@
                     <input type="password" name="admin_password" class="form-control" id="form-control-2" placeholder="Password" data-error="Please enter Correct Password." required>
                     <div class="help-block with-errors"></div>
                   </div>
-                  
+
+                  <?php $getStatus = getDataFromTables('user_status',$status=NULL,$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);?>
                   <div class="form-group">
                     <label for="form-control-3" class="control-label">Choose your status</label>
                     <select id="form-control-3" name="status" class="custom-select" data-error="This field is required." required>
-                      <option value="" selected="selected">Choose your status</option>
-                      <option value="0">Active</option>
-                      <option value="1">In Active</option>
-                      
+                      <option value="">Select Status</option>
+                      <?php while($row = $getStatus->fetch_assoc()) {  ?>
+                          <option value="<?php echo $row['id']; ?>"><?php echo $row['status']; ?></option>
+                      <?php } ?>
                    </select>
                     <div class="help-block with-errors"></div>
                   </div>                  
--- add_users.php
+++ add_users.php
@@ -97,13 +97,14 @@ if (!isset($_POST['submit']))  {
                     <div class="help-block with-errors"></div>
                   </div>
                   
+                  <?php $getStatus = getDataFromTables('user_status',$status=NULL,$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);?>
                   <div class="form-group">
                     <label for="form-control-3" class="control-label">Choose your status</label>
                     <select id="form-control-3" name="status" class="custom-select" data-error="This field is required." required>
-                      <option value="" selected="selected">Choose your status</option>
-                      <option value="0">Active</option>
-                      <option value="1">In Active</option>
-                      
+                      <option value="">Select Status</option>
+                      <?php while($row = $getStatus->fetch_assoc()) {  ?>
+                          <option value="<?php echo $row['id']; ?>"><?php echo $row['status']; ?></option>
+                      <?php } ?>
                    </select>
                     <div class="help-block with-errors"></div>
                   </div>                  
