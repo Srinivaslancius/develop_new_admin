4713f49d95e0231adfb4517f9131801cc0d24893
Srinivaslancius <srinivas@lanciussolutions.com>
2017-08-29 18:53:32 +0530

    index cvhnages

--- manage_webmaster/index.php
+++ manage_webmaster/index.php
@@ -17,15 +17,57 @@
      <div class="site-content">
         <div class="panel panel-default" style="width: 76%;">
           <div class="panel-heading">
-            <h3 class="m-y-0">Login</h3>
+             <div class="clearfix"></div>
+          <!-- display error message -->
+          <?php if(isset($_GET['error']) && $_GET['error']=='fail') { ?>
+          <div class="col-sm-8 col-sm-offset-2">
+            <div class="alert alert-danger alert-icon-bg alert-dismissable" role="alert">
+              <div class="alert-icon">
+                <i class="zmdi zmdi-check"></i>
+              </div>
+              <div class="alert-message">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">
+                    <i class="zmdi zmdi-close"></i>
+                  </span>
+                </button>
+                <strong>Oh snap!</strong> Entered Username or Password is incorrect!.
+              </div>
+            </div>
           </div>
+          <?php } elseif(isset($_GET['error']) && $_GET['error']=='invalid') {  ?>
+          <div class="col-sm-8 col-sm-offset-2">
+            <div class="alert alert-danger alert-icon-bg alert-dismissable" role="alert">
+              <div class="alert-icon">
+                <i class="zmdi zmdi-check"></i>
+              </div>
+              <div class="alert-message">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">
+                    <i class="zmdi zmdi-close"></i>
+                  </span>
+                </button>
+                <strong>Oh snap!</strong> Invalid Request!.
+              </div>
+            </div>
+          </div>
+          <?php } else { ?>
+           <h3 class="m-y-0">Login</h3>
+          <?php } ?>
+          <!-- end error message -->
+           
+
+          </div>
+
+          
           <div class="panel-body" style="height: 250px;">
             <div class="row">
               <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-                <form autocomplete="off" method="post" action="login-script.php">
+                <form autocomplete="off" method="post" action="login-script.php" data-toggle="validator">
                   <div class="form-group">
                     <label for="form-control-1">Email address</label>
-                    <input type="email" autofocus="autofocus" class="form-control" id="form-control-1" placeholder="Email" name="admin_email" data-error="This field is required." required >
+                    <input type="email" class="form-control" id="form-control-2" placeholder="Email" name="admin_email" data-error="Please enter a valid email address." required >
+                    <div class="help-block with-errors"></div>
                   </div>
                   <div class="form-group">
                     <label for="form-control-2">Password</label>
