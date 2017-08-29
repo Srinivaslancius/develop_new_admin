e9cf70200892c62f47c7a8606e5722b82a78ffee
Gunavardhan <vardhan.gujjula@gmail.com>
2017-08-29 11:47:17 +0530

    config file committed

--- admin_includes/config.php
+++ admin_includes/config.php
@@ -21,4 +21,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+
+$base_url = "http://localhost/develop_new_admin/";
+
 ?>
