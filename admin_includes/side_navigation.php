e402357af43cc911989e2976b67bc830cbc81fe6
srinivaslancius <srinivas@lanciussolutions.com>
2017-08-29 12:10:57 +0530

    Changed side navigation and site settings

--- admin_includes/side_navigation.php
+++ admin_includes/side_navigation.php
@@ -19,7 +19,7 @@
                 <li class="<?php if($page_name == 'dashboard.php') { echo "active"; } ?>"><a href="dashboard.php">Dashboards</a></li>
               </ul>
             </li>
-            <li class="with-sub">
+            <li class="<?php if($page_name == 'site_settings.php') { echo "active"; } ?>">
               <a href="site_settings.php" aria-haspopup="true">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-home"></i>
@@ -41,7 +41,7 @@
               </ul>
             </li>  
  
-            <li class="with-sub">
+            <li  class="<?php if($page_name == 'banners.php') { echo "active"; } ?>">
               <a href="banners.php" aria-haspopup="true">
                 <span class="menu-icon">
                    <i class="zmdi zmdi-border-all"></i> 
--- site_settings.php
+++ site_settings.php
@@ -3,7 +3,7 @@
       <div class="site-content">
         <div class="panel panel-default">
           <div class="panel-heading">
-            <h3 class="m-y-0">Form validation</h3>
+            <h3 class="m-y-0">Site Settings</h3>
           </div>
           <div class="panel-body">            
             <div class="row">
