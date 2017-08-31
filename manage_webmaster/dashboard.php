3b7f1a2ecbdad87723d50a771010e5e1c9a18e67
Srinivaslancius <srinivas@lanciussolutions.com>
2017-08-31 12:52:53 +0530

    dashbaord changes

--- manage_webmaster/dashboard.php
+++ manage_webmaster/dashboard.php
@@ -36,7 +36,7 @@
                 <div class="wt-number"><?php echo getRowsCount('orders')?></div>
               </div>
               <div class="wt-icon">
-                <i class="zmdi zmdi-email-open"></i>
+                <i class="zmdi zmdi-shopping-cart-plus zmdi-hc-fw"></i>
               </div>
             </div>
           </div>
@@ -47,7 +47,7 @@
                 <div class="wt-number"><?php echo getRowsCount('products')?></div>
               </div>
               <div class="wt-icon">
-                <i class="zmdi zmdi-email-open"></i>
+                <i class="zmdi zmdi-shopping-basket zmdi-hc-fw"></i>
               </div>
             </div>
           </div>
