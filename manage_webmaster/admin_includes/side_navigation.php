c7cb263c0bdfd66829aa24105cc2078303c488fa
srinivaslancuius <srinivas@lanciussolutions.com>
2017-09-01 12:37:34 +0530

    changes in side_navigation file

--- manage_webmaster/admin_includes/side_navigation.php
+++ manage_webmaster/admin_includes/side_navigation.php
@@ -1,8 +1,8 @@
-    <?php 
-        $currentFile = $_SERVER["PHP_SELF"];
-        $parts = Explode('/', $currentFile);
-        $page_name = $parts[count($parts) - 1];
-    ?>
+<?php
+    $currentFile = $_SERVER["PHP_SELF"];
+    $parts = Explode('/', $currentFile);
+    $page_name = $parts[count($parts) - 1];
+?>
 <div class="site-left-sidebar">
         <div class="sidebar-backdrop"></div>
         <div class="custom-scrollbar">
@@ -12,10 +12,10 @@
               <a href="dashboard.php" aria-haspopup="true">
                 <span class="menu-icon">
                    <i class="zmdi zmdi-view-dashboard zmdi-hc-fw"></i>
-                </span> 
+                </span>
                 <span class="menu-text">Dashboard</span>
               </a>
-            </li>          
+            </li>
             <li class="<?php if($page_name == 'site_settings.php') { echo "active"; } ?>">
               <a href="site_settings.php" aria-haspopup="true">
                 <span class="menu-icon">
@@ -36,44 +36,39 @@
                 <li class="<?php if($page_name == 'admin_users.php' || $page_name == 'add_admin_users.php' || $page_name == 'edit_admin_users.php') { echo "active"; } ?>"><a href="admin_users.php">Admin Users</a></li> 
                 <li class="<?php if($page_name == 'users.php' || $page_name == 'add_users.php' || $page_name == 'edit_users.php') { echo "active"; } ?>"><a href="users.php">Users</a></li>
               </ul>
-            </li>  
- 
+            </li>
             <li  class="<?php if($page_name == 'banners.php' || $page_name == 'add_banners.php' || $page_name == 'edit_banners.php' ) { echo "active"; } ?>">
               <a href="banners.php" aria-haspopup="true">
                 <span class="menu-icon">
                    <i class="zmdi zmdi-collection-image zmdi-hc-fw"></i>
-                </span> 
+                </span>
                 <span class="menu-text">Banners</span>
               </a>
-            </li>  
-
+            </li>
             <li  class="<?php if($page_name == 'content_pages.php' || $page_name == 'add_content_pages.php' || $page_name == 'edit_content_pages.php') { echo "active"; } ?>">
               <a href="content_pages.php" aria-haspopup="true">
                 <span class="menu-icon">
                    <i class="zmdi zmdi-collection-item  zmdi-hc-fw"></i>
-                </span> 
+                </span>
                 <span class="menu-text">Content Pages</span>
               </a>
             </li>
-
             <li  class="<?php if($page_name == 'products.php' || $page_name == 'add_products.php' || $page_name == 'edit_products.php') { echo "active"; } ?>">
               <a href="products.php" aria-haspopup="true">
                 <span class="menu-icon">
                    <i class="zmdi zmdi-collection-image zmdi-hc-fw"></i>
-                </span> 
+                </span>
                 <span class="menu-text">Products</span>
               </a>
             </li>
-
             <li  class="<?php if($page_name == 'orders.php') { echo "active"; } ?>">
               <a href="orders.php" aria-haspopup="true">
                 <span class="menu-icon">
                    <i class="zmdi zmdi-collection-image zmdi-hc-fw"></i>
-                </span> 
+                </span>
                 <span class="menu-text">Orders</span>
               </a>
             </li> 
-
           </ul>
         </div>
       </div>
